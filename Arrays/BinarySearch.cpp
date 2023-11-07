409e3e8784354ff1ed8e9a4c30f04ac76eb432f0
VivekBisht0024 <147581078+VivekBisht0024@users.noreply.github.com>
2023-11-07 14:45:43 +0530

    Add files via upload

---
+++ Arrays/BinarySearch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[] , int n, int key){
+    int s = 0 , e = n - 1;
+    while(s <= e){
+        int mid = s + ( e - s )/2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] < key){
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+int main()
+{
+    int n,key;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        cin>>arr[i];
+    }
+    cin>>key;
+    int ans = binarySearch(arr,n,key);
+    cout<<ans<<endl;
+}
